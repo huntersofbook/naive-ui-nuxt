467a55114176e2e1a99370af58a50e5883a0ac50
productdevbook <hi@productdevbook.com>
2023-07-09 22:11:45 +0300

    fix: upgrade

--- src/module.ts
+++ src/module.ts
@@ -21,7 +21,7 @@ export default defineNuxtModule<ModuleOptions>({
     version,
     configKey: 'naiveUI',
     compatibility: {
-      nuxt: '^3.2.2',
+      nuxt: '^3.6.1',
     },
   },
   setup(moduleOptions, nuxt) {
